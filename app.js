88c948b9d8edc227a2adb75a9400cb3de93a0cb8
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-02-28 19:35:57 -0800

    Fix:replace import.meta.dirname for dirname for Node compatibility

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3004;
