ba64048c2edf58881e2e303116d334341a0ba810
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-02-28 19:48:22 -0800

    typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { dirnname, join } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
